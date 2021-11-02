4f16a29b6552a62bf7d2441c08b402a67a926154
kamalnorizan <kamalnorizan@gmail.com>
2021-11-02 12:08:57 +0800

    layout

--- lib/home.dart
+++ lib/home.dart
@@ -9,13 +9,18 @@ class Home extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text('Home Page'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text('Text 1'),
+              Text('Text 2'),
+              Text('Text 3')
+            ],
           ),
-        ),
+        )
       ),
     );
   }
