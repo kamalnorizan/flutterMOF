47a67652abb7ab093bc9e847c377b2c8da88b270
kamalnorizan <kamalnorizan@gmail.com>
2021-11-02 12:34:54 +0800

    row

--- lib/home.dart
+++ lib/home.dart
@@ -12,12 +12,20 @@ class Home extends StatelessWidget {
           title: const Text('Home Page'),
         ),
         body: Container(
+          color: Colors.brown,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Text 1'),
-              Text('Text 2'),
-              Text('Text 3')
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Center(child: Text('Text 1')),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text('Text 2'),
+                  Text('Text 3')
+                ],
+              ),
+
             ],
           ),
         )
