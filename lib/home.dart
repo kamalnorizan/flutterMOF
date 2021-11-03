27e13b949a85cd96949aea3dce767a1e1fda5f88
kamalnorizan <kamalnorizan@gmail.com>
2021-11-03 11:33:02 +0800

    convert products to list

--- lib/home.dart
+++ lib/home.dart
@@ -2,8 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:fluttertutorial/models/product.dart';
 import 'package:fluttertutorial/widgets/productbox.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Product> products = [
+    Product(
+        name: 'iPhone',
+        description: 'iPhone is the most expensive phone ever',
+        price: 4000,
+        imageName: 'iPhone'),
+    Product(
+        name: 'Laptop',
+        description: 'Laptop is most productive development tool',
+        price: 2500,
+        imageName: 'Laptop'),
+    Product(
+      name: 'Tablet',
+      description: 'Tablet is most productive development tool',
+      price: 2300,
+      imageName: 'Tablet',
+    ),
+    Product(
+      name: 'Pendrive',
+      description: 'Pendrive is a storage medium',
+      price: 20,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
